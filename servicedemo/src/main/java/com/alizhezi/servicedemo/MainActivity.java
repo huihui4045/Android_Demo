d98a19b9aca2e0ca889c088ce706a0903c7121b2
千涯秋瑟 <1476265623@qq.com>
2017-11-29 22:44:53 +0800

    Service demo

--- servicedemo/src/main/java/com/alizhezi/servicedemo/MainActivity.java
+++ servicedemo/src/main/java/com/alizhezi/servicedemo/MainActivity.java
@@ -1,22 +1,53 @@
 package com.alizhezi.servicedemo;
 
+import android.content.ComponentName;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private Intent intent;
+
+    private ServiceConnection connection;
+
+    private ServiceDemo serviceDemo;
+
+    private String TAG=this.getClass().getSimpleName();
+    private TextView viewById;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        viewById = ((TextView) findViewById(R.id.text));
+
         intent = new Intent(this, ServiceDemo.class);
 
         intent.putExtra("data", "来自Activity");
+
+
+        connection=new ServiceConnection() {
+            @Override
+            public void onServiceConnected(ComponentName name, IBinder service) {
+
+                 serviceDemo = ((ServiceDemo.LocalBinder) service).getService();
+
+                Log.d(TAG,"onServiceConnected");
+            }
+
+            @Override
+            public void onServiceDisconnected(ComponentName name) {
+
+                Log.d(TAG,"onServiceDisconnected:"+name.getClassName());
+            }
+        };
     }
 
 
@@ -28,5 +59,20 @@ public class MainActivity extends AppCompatActivity {
     public void stopService(View view) {
         stopService(intent);
 
+    }
+
+    public void bindService(View view){
+
+        bindService(intent,connection,BIND_AUTO_CREATE);
+    }
+
+    public void unBindService(View view){
+
+        unbindService(connection);
+    }
+
+    public void getData(View view){
+
+        viewById.setText(""+serviceDemo.getCount());
     }
 }
--- servicedemo/src/main/java/com/alizhezi/servicedemo/ServiceDemo.java
+++ servicedemo/src/main/java/com/alizhezi/servicedemo/ServiceDemo.java
@@ -2,6 +2,7 @@ package com.alizhezi.servicedemo;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Binder;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.util.Log;
@@ -11,34 +12,98 @@ import android.util.Log;
  */
 
 public class ServiceDemo extends Service {
-    private String TAG=this.getClass().getSimpleName();
+    private String TAG = this.getClass().getSimpleName();
+
+    private LocalBinder mLocalBinder;
+
+    private int count;
+
+    private boolean isExit = false;
+
+    private Thread mThread;
+
+    public class LocalBinder extends Binder {
+
+        public ServiceDemo getService() {
+
+            return ServiceDemo.this;
+        }
+    }
+
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
-        return null;
+
+        Log.d(TAG, "onBind");
+        return mLocalBinder;
+    }
+
+    @Override
+    public boolean onUnbind(Intent intent) {
+        Log.d(TAG, "onUnbind");
+        return super.onUnbind(intent);
+    }
+
+    public void stop() {
+        isExit = true;
+    }
+
+    public int getCount() {
+
+        return count;
     }
 
     @Override
     public void onCreate() {
         super.onCreate();
 
-        Log.d(TAG,"onCreate");
+        Log.d(TAG, "onCreate");
+
+        mLocalBinder = new LocalBinder();
+
+        mThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                while (!isExit) {
+
+                    count++;
+
+                    try {
+                        Thread.sleep(1000);
+
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                    Log.e(TAG,"数值："+count);
+                    if (count==10){
+                        stopSelf();
+                    }
+
+                }
+            }
+        });
+
+        mThread.start();
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         //Log.d(TAG,"onStartCommand:"+intent.getStringExtra("data"));
 
-        int flag=super.onStartCommand(intent, flags, startId);
+        int flag = super.onStartCommand(intent, flags, startId);
 
-        Log.d(TAG,"onStartCommand:"+intent.getStringExtra("data")+"   "+flag);
+        Log.d(TAG, "onStartCommand:" + intent.getStringExtra("data") + "   " + flag);
         return START_REDELIVER_INTENT;
     }
 
     @Override
     public void onDestroy() {
 
-        Log.d(TAG,"onDestroy");
+        Log.d(TAG, "onDestroy");
+
+        isExit=true;
 
         super.onDestroy();
     }
