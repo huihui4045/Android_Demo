48a01789136898f2cb2874c0926ae197a4c88ee7
DESKTOP-DMBJUVJ\gavin <gavin6116@gmail.com>
2017-12-06 09:16:19 +0800

    HandleThread

--- android_viewdemo/src/main/java/com/huihui/viewdemo/MainActivity.java
+++ android_viewdemo/src/main/java/com/huihui/viewdemo/MainActivity.java
@@ -1,13 +1,33 @@
 package com.huihui.viewdemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private View view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        view = findViewById(R.id.myview);
+
+
+        //Activity
+    }
+
+    public void requestLayout(View view) {
+
+        this.view.requestLayout();
+    }
+
+    public void invalidate(View view) {
+
+        this.view.invalidate();
+
+        view.postInvalidate();
     }
 }
--- android_viewdemo/src/main/java/com/huihui/viewdemo/MyView.java
+++ android_viewdemo/src/main/java/com/huihui/viewdemo/MyView.java
@@ -4,13 +4,17 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 
 /**
  * Created by molu_ on 2017/12/4.
  */
 
-public class MyView extends View{
+public class MyView extends View {
+
+    private String TAG=this.getClass().getSimpleName();
+
     public MyView(Context context) {
         super(context);
     }
@@ -26,15 +30,21 @@ public class MyView extends View{
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+
+        Log.e(TAG,"onMeasure");
     }
 
     @Override
     protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
         super.onLayout(changed, left, top, right, bottom);
+
+        Log.e(TAG,"onLayout");
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        Log.e(TAG,"onDraw");
     }
 }
